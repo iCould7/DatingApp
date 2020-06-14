080d87d6d8b0f8834bc12c4178c62d33e55cdc74
aikutto <aslanaykut4@gmail.com>
2020-06-14 21:08:13 +0300

    Error interceptor providor added

--- DatingApp.API/Controllers/AuthController.cs
+++ DatingApp.API/Controllers/AuthController.cs
@@ -45,6 +45,8 @@ namespace DatingApp.API.Controllers
         [HttpPost("login")]
         public async Task<IActionResult> Login(UserForLoginDto loginDto)
         {
+            throw new Exception("NO FUCK!");
+
             loginDto.Username = loginDto.Username.ToLower();
 
             var userFromRepo = await _repo.Login(loginDto.Username, loginDto.Password);
